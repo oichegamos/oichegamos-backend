ecc51dc8235de59233a96f8c4200860a3b76a8f4
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-04-13 15:45:35 -0300

    Tenta mudar local da vendor

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return json_encode(["message" => "API Oi, Chegamos!"]);
 });
-
-Route::post('/post-deploy', function () {
-
-    //unpack vendor.zip
-    $zip = new ZipArchive;
-    if ($zip->open('vendor.zip') === TRUE) {
-        $zip->extractTo('./');
-        $zip->close();
-
-        http_response_code(200);
-        exit;
-    }
-});
