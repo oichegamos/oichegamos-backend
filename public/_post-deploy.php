8fb8a834f7813ccea7f5c816044438cf9e73e5ff
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-04-13 15:54:32 -0300

    print files

--- public/_post-deploy.php
+++ public/_post-deploy.php
@@ -1,18 +1,41 @@
 <?php
 
 function extractVendorZip() {
-    $vendorZip = './vendor.zip';
 
-    if (file_exists($vendorZip)) {
-        $zip = new ZipArchive;
+    // $path = './';
 
-        if ($zip->open($vendorZip) === TRUE) {
-            $zip->extractTo('./');
-            $zip->close();
+    echo('------- ./');
+    printFolderFiles('./');
 
-            unlink($vendorZip);
-        }
+    echo('------- ././');
+    printFolderFiles('././');
+
+    echo('------- ./././');
+    printFolderFiles('./././');
+
+
+    // $vendorZip = './vendor.zip';
+
+    // if (file_exists($vendorZip)) {
+    //     $zip = new ZipArchive;
+
+    //     if ($zip->open($vendorZip) === TRUE) {
+    //         $zip->extractTo('./');
+    //         $zip->close();
+
+    //         unlink($vendorZip);
+    //     }
+    // }
+}
+
+function printFolderFiles($folder) {
+    $dir = dir($path);
+
+    while ($file = $dir->read()) {
+        echo "found file: " . $file;
     }
+
+    $dir->close();
 }
 
 extractVendorZip();
