617f29121698339af35fb71cebc3a668f47fe806
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-04-14 18:51:26 -0300

    Downgrade do laravel para versão 8

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
-Route::get('/', function () {
-    return json_encode(['message' => 'its working']);
-});
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-Artisan::command('optimize', function () {
-    // $this->info("Sending email to: {$user}!");
-    return json_encode(['message' => 'optimized']);
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
