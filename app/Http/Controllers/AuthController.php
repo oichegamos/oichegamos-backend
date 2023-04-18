e52e74eb301003b04089dde8db1a769ad0b52749
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-04-18 15:04:07 -0300

    Adiciona jwt

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json(compact('token'));
+    }
+
+    public function register(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json(['message' => 'User created successfully'], 201);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/foo', function () {
     return ['message' => 'working!'];
+});
+
+Route::post('auth/signIn', [AuthController::class, 'login']);
+Route::post('auth/signUp', [AuthController::class, 'register']);
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
