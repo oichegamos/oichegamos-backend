61183d93f26b28cfb3f4f27de6a50fe1ec0fd056
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-04-18 18:12:11 -0300

    Cria padrão para controllers e services

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,13 +1,44 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $service;
+    protected $model;
+
+    public function index(Request $request)
+    {
+        $paginate = filter_var($request->query('paginate', true), FILTER_VALIDATE_BOOLEAN);
+        return $this->service->index($paginate);
+    }
+
+    public function show($id)
+    {
+        return $this->service->show($id);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->model->rules());
+        return $this->service->store($request);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, $this->model->rules());
+        return $this->service->update($request, $id);
+    }
+
+    public function destroy($id)
+    {
+        return $this->service->destroy($id);
+    }
 }
