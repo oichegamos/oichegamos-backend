a611d67de4748f61a013d01f84a14fe0797d0b70
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-04-19 20:51:25 -0300

    Optimiza laravel após descompactar a vendor

--- public/_post-deploy.php
+++ public/_post-deploy.php
@@ -22,21 +22,37 @@ function extractVendorZip() {
 
 function deleteFolder($directory) {
     if (is_dir($directory)) {
-      $contents = scandir($directory);
-
-      foreach ($contents as $item) {
-        if ($item != "." && $item != "..") {
-          $itemPath = $directory . "/" . $item;
-          if (is_dir($itemPath)) {
-            deleteFolder($itemPath);
-          } else {
-            unlink($itemPath);
-          }
+        $contents = scandir($directory);
+    
+        foreach ($contents as $item) {
+            if ($item != "." && $item != "..") {
+                $itemPath = $directory . "/" . $item;
+                if (is_dir($itemPath)) {
+                    deleteFolder($itemPath);
+                } else {
+                    unlink($itemPath);
+                }
+            }
         }
-      }
 
-      rmdir($directory);
+        rmdir($directory);
     }
-  }
+}
+
+function callArtisan($command) {
+    exec($command, $output, $return_var);
+
+    if ($return_var !== 0) {
+        echo 'Erro ao executar rotina artisan';
+    }
+}
+
+function optimizeLaravel() {
+    callArtisan("php artisan cache:clear");
+    callArtisan("php artisan route:clear");
+    callArtisan("php artisan config:clear");
+    callArtisan("php artisan optimize");
+}
 
 extractVendorZip();
+optimizeLaravel();
