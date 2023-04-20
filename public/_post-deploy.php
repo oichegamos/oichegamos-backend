fc39479d552c522923bc0133f54e76ad8dc5883e
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-04-20 10:00:55 -0300

    Ajusta post deploy para limpar o cache

--- public/_post-deploy.php
+++ public/_post-deploy.php
@@ -1,4 +1,6 @@
 <?php
+// require_once __DIR__ . '../../vendor/autoload.php';
+// use Symfony\Component\Process\Process;
 
 function extractVendorZip() {
     $path = '../';
@@ -15,7 +17,7 @@ function extractVendorZip() {
             $zip->close();
             unlink($vendorZip);
 
-            echo 'done!';
+            // echo 'done!';
         }
     }
 }
@@ -39,20 +41,68 @@ function deleteFolder($directory) {
     }
 }
 
-function callArtisan($command) {
-    exec($command, $output, $return_var);
+// function callArtisan($command) {
+//     // exec($command, $output, $return_var);
 
-    if ($return_var !== 0) {
-        echo 'Erro ao executar rotina artisan';
-    }
-}
+//     // if ($return_var !== 0) {
+//     //     echo 'Erro ao executar rotina artisan';
+//     // }
+//     $process = new Process(['php', 'artisan', $command]);
+//     $process->run();
+
+//     if (!$process->isSuccessful()) {
+//         var_dump($process);
+//         echo 'Erro ao executar rotina artisan';
+//     }
+// }
 
 function optimizeLaravel() {
-    callArtisan("php artisan cache:clear");
-    callArtisan("php artisan route:clear");
-    callArtisan("php artisan config:clear");
-    callArtisan("php artisan optimize");
+    // callArtisan("cache:clear");
+    // callArtisan("route:clear");
+    // callArtisan("config:clear");
+    // callArtisan("optimize");
+
+
+    // Diretório raiz do seu projeto Laravel
+    $path = '../';
+
+    // Apaga todos os arquivos do diretório storage/framework/cache
+    $cache_dir = $path . '/storage/framework/cache';
+    $files = glob($cache_dir . '/*');
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
+
+    // Apaga todos os arquivos do diretório storage/framework/sessions
+    $sessions_dir = $path . '/storage/framework/sessions';
+    $files = glob($sessions_dir . '/*');
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
+
+    // Apaga todos os arquivos do diretório storage/framework/views
+    $views_dir = $path . '/storage/framework/views';
+    $files = glob($views_dir . '/*');
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
+
+    // Apaga todos os arquivos do diretório bootstrap/cache
+    $bootstrap_cache_dir = $path . '/bootstrap/cache';
+    $files = glob($bootstrap_cache_dir . '/*');
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
 }
 
 extractVendorZip();
 optimizeLaravel();
+echo 'done!';
