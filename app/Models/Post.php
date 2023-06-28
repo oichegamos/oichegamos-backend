4bb41b2beff0a3a7ce9336d43ca9e71fc382898e
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-06-28 20:22:45 -0300

    Resolve post problems

---
+++ app/Models/Post.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $with = ['image', 'category'];
+
+    protected $fillable = [
+        'title',
+        'content',
+
+        'category_id',
+        'image_id',
+    ];
+
+    protected $hidden = [
+        'category_id',
+        'image_id',
+    ];
+
+    public function rules(){
+        return [
+            'title' => 'required',
+            'content' => 'required',
+        ];
+    }
+
+    public function image() {
+        return $this->belongsTo(Image::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+}
